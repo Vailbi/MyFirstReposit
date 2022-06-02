f61c47def07b303be7758ef00c0e322fb8fb4888
Fedor <vailbi94@gmail.com>
2022-06-02 23:29:05 +0300

    финал

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+from turtle import width
+
+
 number_of_pixel = int(input())
 n=1
 size=list()
@@ -7,9 +10,14 @@ while n <= number_of_pixel/2:
     n +=1
 size= size+[number_of_pixel]
 size2=size[::-1]
-for length in size:
-    for width in size2:
+zip(size,size2)
+for lenght,width in zip(size,size2):
+    print (str(lenght)+" x",width)
+
+
+#for length in size:
+ #   for width in size2:
         
 
-        print(length, width)
+  #      print(length, width)
 #print(size,size2)
