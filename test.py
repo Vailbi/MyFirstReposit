44f01ee99fc236e47bdbb6ef3d15678f640b3bb1
Fedor <vailbi94@gmail.com>
2022-06-02 23:17:10 +0300

    финальная часть

--- test.py
+++ test.py
@@ -5,4 +5,11 @@ while n <= number_of_pixel/2:
     if number_of_pixel%n==0:
         size.append(n)
     n +=1
-print(size)
+size= size+[number_of_pixel]
+size2=size[::-1]
+for length in size:
+    for width in size2:
+        
+
+        print(length, width)
+#print(size,size2)
