9b11eb098f4d9a39ce1dc935b357840bcfc173d6
Fedor <vailbi94@gmail.com>
2022-06-02 21:34:38 +0300

    первая часть задачи по размере картинки

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-array=list(range(0,11))
-sum=0
-for x in array:
-    sum=sum+x
-print(sum)
+number_of_pixel = int(input())
+n=1
+while n <= number_of_pixel/2:
+    if number_of_pixel%n==0:
+        print(n)
+    n +=1
